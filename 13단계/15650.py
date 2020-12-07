4e0edb2d5a5328ac5561ec68a1433ebd515e4cee
YeChanSong <davidsyc1170@gmail.com>
2020-12-07 22:21:34 +0900

    9663까지

---
+++ 13단계/15650.py
@@ -0,0 +1,8 @@
+import sys, itertools
+
+N,M = map(int,sys.stdin.readline().strip().split())
+li = list(map(list,itertools.combinations([i for i in range(1,N+1)],M)))
+for i in sorted(li):
+    for j in i:
+        print(j,end=' ')
+    print()
