0f3f45ff195db44cbe31722f54568e9a67e24474
YeChanSong <davidsyc1170@gmail.com>
2021-01-26 15:46:48 +0900

    11444 수정

--- 19단계/11444.py
+++ 19단계/11444.py
@@ -1,31 +1,32 @@
 import sys
-sys.setrecursionlimit(10000000)
+
 '''
-Fn = F(k+1)*F(n-k) + F(k)*F(n-k-1)
-F5 = 5 = F3*F3 + F2*F2 = 2*2 + 1*1
-Fn(mod p) = F(k+1)(mod p)*F(n-k)(mod p) + F(k)(mod p)*F(n-k-1)(mod p)
+F(n)   = (1 1) ^ n-1 (F(1))
+F(n-1) = (1 0)       (F(0))
 '''
 
-def dq(idx):
-    global cache
-
-    tmp = cache.get(idx%1000000007)
-    if tmp != None:
-        return tmp
+def pow(idx):
+    global a
+    if idx == 0:
+        return [[1,0],[0,1]]
     
-    if idx%2 == 0:
-        tmp = (dq(idx-1)%1000000007 + dq(idx-2)%1000000007)%1000000007
-        cache.setdefault(idx,tmp)
-        return tmp
-    a = dq(idx//2+1)%1000000007
-    b = dq(idx//2)%1000000007
-    tmp = ((a**2)%1000000007+(b**2)%1000000007)%1000000007
-    cache.setdefault(idx,tmp)
+    if idx%2 == 1:
+        return mul(a,pow(idx-1))
+    tmp = pow(idx//2)
+    return mul(tmp,tmp)
+    
+def mul(a,b):
+    tmp = [[0,0],[0,0]]
+    for i in range(2):
+        for j in range(2):
+            ttmp = 0
+            for k in range(2):
+                ttmp += a[i][k] * b[k][j]
+            tmp[i][j] = ttmp%1000000007
     return tmp
-    
+
 n = int(sys.stdin.readline().strip())
-cache = dict()
-cache.setdefault(0,0)
-cache.setdefault(1,1)
-ans = dq(n)
+a =[[1,1],[1,0]]
+npow = pow(n-1)
+ans = npow[0][0]%1000000007
 print(ans)
