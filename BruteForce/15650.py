23d6d263c418f62c9fd66f3d065d6cb8cbf434c2
YeChanSong <davidsyc1170@gmail.com>
2021-03-30 21:04:11 +0900

    15650 추가

---
+++ BruteForce/15650.py
@@ -0,0 +1,16 @@
+import sys
+
+N,M = map(int,sys.stdin.readline().strip().split())
+numList = list(i for i in range(1,N+1))
+def recur(idx, hist):
+    if idx == M:
+        print(' '.join(map(str,hist)))
+        return
+    for i in numList:
+        if len(hist) > 0 and i<= hist[-1]:
+            continue
+        tmp = hist.copy()
+        tmp.append(i)
+        recur(idx+1,tmp)
+
+recur(0,[])
