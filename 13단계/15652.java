dfd6862afa8cc37b16405a39a9c289b5f34c2b67
YeChanSong <davidsyc1170@gmail.com>
2021-07-17 18:42:06 +0900

    15652.java

---
+++ 13단계/15652.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringBuilder sb = new StringBuilder(8192);
+    static int N,M;
+    static Stack<Integer> stack = new Stack<>();
+    public static void main(String[] args) throws IOException{
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] tmp = br.readLine().split(" ");
+        N = Integer.parseInt(tmp[0]);
+        M = Integer.parseInt(tmp[1]);
+        recur(0,1);
+        bw.write(sb.toString());
+        bw.flush();bw.close();br.close();
+    }
+    public static void recur(int cnt, int idx){
+        if(cnt == M){
+            for(Object o: stack.toArray()){
+                sb.append(o).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for(int i=idx;i<=N;i++){
+            stack.push(i);
+            recur(cnt+1,i);
+            stack.pop();
+        }
+    }
+}
