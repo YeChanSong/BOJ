4590e8c87d66d31710fd523d8f1e6387f9a5c1d9
YeChanSong <davidsyc1170@gmail.com>
2022-06-11 15:01:59 +0900

    solve: 10926 [kt]

---
+++ 1단계/10926.kt
@@ -0,0 +1,9 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val data = br.readLine()
+    println("${data}??!")
+}
